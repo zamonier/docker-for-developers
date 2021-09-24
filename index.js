aeccbc07e756ef1635a43bd7e95da16e5fc2d035
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2021-09-24 17:27:41 +0300

    second commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I love docker!");
+    res.json("Our build will succeed!");
 });
 
 app.listen(PORT, () => {
